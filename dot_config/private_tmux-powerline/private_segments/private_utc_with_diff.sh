d3d33a3c7f40d913915907b60a90145ed8f29c72
Javier Guaje <jrguajeg@gmail.com>
2026-03-15 23:22:20 -0400

    style(tmux-powerline): use rounded thin separators and consolidate UTC status

--- dot_config/private_tmux-powerline/private_segments/private_utc_with_diff.sh
+++ dot_config/private_tmux-powerline/private_segments/private_utc_with_diff.sh
@@ -2,10 +2,11 @@
 # Prints the current time in UTC with difference with local time.
 
 run_segment() {
+	utc_date=$(date -u +"%m-%d")
 	utc_time=$(date -u +"%H:%M")
 	diff=$(date +"%z")
 	# Format +HHMM to +HH:MM
 	formatted_diff="${diff:0:3}:${diff:3:2}"
-	echo "${utc_time} UTC (${formatted_diff})"
+	echo "${utc_date} ${utc_time} UTC (${formatted_diff})"
 	return 0
 }
